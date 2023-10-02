0de9fa074a4c7a0ca0f97d63edc4370c19581abb
nil <nillinvanillin@gmail.com>
2023-10-02 16:08:00 -0400

    Wow!
    It Spin!

--- Code/CounterRotate.cs
+++ Code/CounterRotate.cs
@@ -1,10 +1,10 @@
 using Godot;
 using System;
 
-public partial class player : Node2D
+public partial class CounterRotate : Node2D
 {
 	[Export]
-	public float rotation = 0;
+	Node2D parent;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,6 +14,6 @@ public partial class player : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.Transform = this.Transform.Rotated(rotation * (float)delta);
+		this.Transform = new Transform2D(-parent.Transform.Rotation, this.Transform.Origin);
 	}
 }
